2959fffb48b912d8a0bf3d1aac34ccee0ec68638
Pablo Arias <JuanPablo.Arias@team.neustar>
2021-06-12 14:21:27 -0600

    init

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,2 +1,11 @@
 class BudgetList:
-    pass
+    
+    def __init__(self, budget):
+        self.budget = budget
+        self.sum_expenses = 0
+        self.expenses = []
+        self.sum_overages = 0
+        self.overages = []
+
+    def append(self, item):
+        pass
