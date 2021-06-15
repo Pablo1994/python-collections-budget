12aa6d92808f9a616347eca2bedc38034161b96c
Pablo Arias <JuanPablo.Arias@team.neustar>
2021-06-15 09:28:44 -0600

    expensecategories

--- budget/Expense.py
+++ budget/Expense.py
@@ -48,3 +48,4 @@ class Expenses():
                                 or x.category == 'Mortgage' ]
         food_expenses = [x for x in self.list if x.category == 'Groceries' or x.category == 'Eating Out']
         unnecessary_expenses = set(self.list).difference(necessary_expenses, food_expenses)
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
